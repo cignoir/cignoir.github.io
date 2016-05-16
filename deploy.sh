f74770503c741edff9967ad071f8b28ee1285b77
cignoir <cignoir@gmail.com>
2016-05-16 14:24:38 +0900

    no description

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
-updated_at=`date "+%Y-%m-%dT%H:%M:%S+09:00"`
-echo $updated_at
+ci_message="no description"
+if [ $# eq 1 ]; then
+  ci_message="$1"
+fi
+echo $ci_message
 
-bundle exec html2haml --unix-newlines index.html.haml index.html
+bundle exec haml index.html.haml index.html
 
 git add -A
-git commit -m "Updated at $updated_at"
+git commit -m "$ci_message"
 git push -u origin master
